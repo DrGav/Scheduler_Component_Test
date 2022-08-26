e1fca73b11f10da1f642817afbfc09271e3f72f1
Gavin Williams <dr.gpwilliams@gmail.com>
2022-08-26 08:23:02 +0100

    Add project files.

---
+++ Scheduler_Component_Test/Appointment.cs
@@ -0,0 +1,12 @@
+﻿namespace Scheduler_Component_Test
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string? Text { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int AppointmentOwnerId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
